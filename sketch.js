57f42e759dc483f572175276ebfb93a82b3f3abe
SurtMcGert <harry.ayre@outlook.com>
2023-06-04 17:17:25 +0100

    for some reason, that merge, merged from dev, into this branch idk

--- sketch.js
+++ sketch.js
@@ -9,20 +9,5 @@ function setup() {
 
 function draw() {
     background(0);
-    for (let wall of b) {
-        wall.draw();
-    }
-    particle.update(mouseX, mouseY);
-    particle.show();
-    particle.look(b);
-    // ray.show();
-    // ray.lookAt(mouseX, mouseY);
-
-    //   let pt = ray.cast(b);
-    //   if (pt) {
-    //       fill(255);
-    //       ellipse(pt.x, pt.y, 8, 8);
+    g.draw();
 }
-
-
-
