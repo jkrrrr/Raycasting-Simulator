54d938ac764772f4dcc2550469a8af084effe602
SurtMcGert <harry.ayre@outlook.com>
2023-06-04 17:03:26 +0100

    implemented terrain scrolling

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ function setup() {
     let width = 500;
     let height = 500;
     createCanvas(width, height);
-    this.g = new TerrainGen(width, height);
-    this.g.generatePath();
+    g = new TerrainGen(width, height);
+    frameRate(20);
 }
 
 function draw() {
     background(0);
-    this.g.draw();
+    g.draw();
 }
